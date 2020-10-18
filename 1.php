cce700e36508d4b8608dde1966e155341b5b6854
ivycrow <73049192+ivycrow@users.noreply.github.com>
2020-10-18 12:38:42 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -4,9 +4,7 @@ function data($name,$age) {
 	$data = array(
 		"name" => $name, 
 		"age" => $age,
-		"hobbies" => array("Ngoding","Main Musik","Videografi"),
 		"is_married" => false,
-		"skills" => array(0 => array("skill_name" => "PHP", "level" => "beginner"), 1 => array("skill_name" => "NodeJS", "level" => "beginner")),
 		"interest_in_coding" => true
 		);
 	$json = json_encode($data);
